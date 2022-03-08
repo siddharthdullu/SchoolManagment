b5ff23a28c642c2fc9ad87ad79ef1ce340a5eb42
Siddharth <siddharth_dullu@persistent.com>
2022-03-08 16:30:10 +0530

    Add project files.

---
+++ SchoolManagment/Startup.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using SchoolManagment.Models;
+
+[assembly: OwinStartupAttribute(typeof(SchoolManagment.Startup))]
+namespace SchoolManagment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            createRolesandUsers();
+        }
+        public void createRolesandUsers()
+        {
+            var context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser
+                {
+                    UserName = "admin",
+                    Email = "admin@schmang.com",
+                    BirthDate = System.DateTime.Now
+                };
+                var password = "password";
+
+                var usr = userManager.Create(user, password);
+
+                if (usr.Succeeded)
+                {
+                    var result = userManager.AddToRole(user.Id, "Admin");
+                }
+            }
+            if (!roleManager.RoleExists("Teacher"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Teacher";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Supervisor"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Supervisor";
+                roleManager.Create(role);
+            }
+        }
+
+        
+
+    }
+}
